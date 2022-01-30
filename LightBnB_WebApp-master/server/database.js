82e916b27e84001f609f064969cc272070a6db86
Jorawer Grewal <joegrewal20@gmail.com>
2022-01-29 21:57:40 -0800

    Add filter by owner_id to getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -133,7 +133,16 @@ exports.getAllReservations = getAllReservations;
 
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    if (queryParams.length > 1) {
+      queryString += `AND owner_id = $${queryParams.length}`;
+    } else {
+      queryString += `WHERE owner_id = $${queryParams.length}`;
+    }
   }
 
   queryParams.push(limit);
