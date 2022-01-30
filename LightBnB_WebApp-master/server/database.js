79aca6c025f4e1b2125238b8f86adf330a65efd3
Jorawer Grewal <joegrewal20@gmail.com>
2022-01-29 22:11:26 -0800

    Add filter by min and max price for getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -145,6 +145,24 @@ exports.getAllReservations = getAllReservations;
     }
   }
 
+  if (options.minimum_price_per_night) {
+    queryParams.push((options.minimum_price_per_night * 100));
+    if (queryParams.length > 1) {
+      queryString += `AND cost_per_night > $${queryParams.length} `;
+    } else {
+      queryString += `WHERE cost_per_night > $${queryParams.length} `;
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push((options.maximum_price_per_night * 100));
+    if (queryParams.length > 1) {
+      queryString += `AND cost_per_night < $${queryParams.length} `;
+    } else {
+      queryString += `WHERE cost_per_night < $${queryParams.length} `;
+    }
+  }
+
   queryParams.push(limit);
   queryString += `
   GROUP BY properties.id
